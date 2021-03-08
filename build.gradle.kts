adc6eb79c20552bfeada1d7995e5c7e1176c3d1f
Max <msal.coding@gmail.com>
2021-03-08 22:12:40 +0100

    add dependency update checker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import java.net.URI
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
@@ -14,6 +15,22 @@ buildscript {
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+    }
+}
+
+// dependency-update-checker: gradle dependencyUpdates
+plugins {
+    id("com.github.ben-manes.versions") version "0.38.0"
+}
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
 
